709c3fa5faa29b45ce6e05c6e2c15f8556fa58ca
David Chua <dp_chua@hotmail.com>
2021-03-04 01:22:52 -0500

    Pretasks

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ import { fifaData } from './fifa.js';
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
+const final2014 = fifaData.filter(function(item) {
+    return item ["Year"] === 2014 && item["Stage"] === "Final";
+});
 
 //(a) Home Team name for 2014 world cup final
+console.log(final2014[0]["Home Team Name"]);
 
 //(b) Away Team name for 2014 world cup final
+console.log(final2014[0]["Away Team Name"]);
 
 //(c) Home Team goals for 2014 world cup final
+console.log(final2014[0]["Home Team Goals"]);
 
 //(d) Away Team goals for 2014 world cup final
+console.log(final2014[0]["Home Team Goals"]);
 
 //(e) Winner of 2014 world cup final */
+console.log(final2014[0]["Win conditions"]);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
